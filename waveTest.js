6c2f8c34439f5e5a91f5877c9a24043bdbc54a1c
christian-carroll <christian.carroll@ibm.com>
2017-07-20 18:11:50 +0000

    adding wave test

---
+++ waveTest.js
@@ -0,0 +1,23 @@
+var TJBot = require('tjbot');
+var config = require('./config');
+
+// obtain our credentials from config.js
+var credentials = config.credentials;
+
+// obtain user-specific config
+var WORKSPACEID = config.conversationWorkspaceId;
+
+// these are the hardware capabilities that TJ needs for this recipe
+var hardware = ['microphone', 'speaker', 'camera', 'led', 'servo'];
+
+// set up TJBot's configuration
+var tjConfig = {
+    log: {
+        level: 'verbose'
+    }
+};
+
+// instantiate our TJBot!
+var tj = new TJBot(hardware, tjConfig, credentials);
+
+tj.wave();
